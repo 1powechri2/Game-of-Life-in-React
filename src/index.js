ec05ee8119a43334873b10ebfae429828da8c174
Chris Powell <chris.ian.powell@gmail.com>
2021-01-13 11:56:15 -0700

    Moves game code into own dir

--- src/index.js
+++ src/index.js
@@ -2,13 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const game = require('./game-of-life-code/game_of_life');
+const gOF = new game(100,100);
+const firstWorld = gOF.world();
+
+class Cell extends React.Component {
+  render() {
+    return (
+      <span className='cell' style={{backgroundColor: this.props.color}}>
+        <span className='bullseye'></span>
+      </span>
+    )
+  }
+}
+
+class World extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: firstWorld,
+    }
+  }
+
+
+  renderCell(val) {
+    var color = null;
+    if (val == 0) {
+      color = 'black'
+    } else {
+      color = '#EE34D2'
+    }
+    return (
+      <Cell value={val} color={color} />
+    );
+  }
+
+  evolveGame() {
+    var nextGen = gOF.readWorld(this.state.cells);
+    this.setState({cells: nextGen});
+  }
+
+  render() {
+    setInterval(() => {
+      this.evolveGame()
+    } , 500)
+
+    var cells = this.state.cells.flat();
+
+    return(
+      <div className='world'>
+        {cells.map(cell => (
+          this.renderCell(cell)
+        ))}
+      </div>
+    );
+  }
+}
 
 class GameOfLife extends React.Component {
   render() {
     return (
-      <div className="game-of-life">
-        <h1>HELLO THERE!</h1>
-      </div>
+      <section className="game-of-life">
+        <World />
+      </section>
     )
   }
 }
